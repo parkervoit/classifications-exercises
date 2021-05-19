87f39dec2648d03111e431facf994ee8b278e7f5
parkervoit <parker.r.voit@gmail.com>
2021-05-19 12:26:22 -0500

    added explore.py file and modified prepare.py

--- prepare.py
+++ prepare.py
@@ -3,32 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-def prep_iris(iris_df):
+def prep_iris(iris_df, dummies):
     '''
     Takes in the iris_df, drops species id, and measurement id, then adds a dummy
     variable column. It then returns the iris_df cleaned as iris_df
     '''
     iris_df = iris_df.drop(columns = ['species_id', 'measurement_id'])
     iris_df = iris_df.rename(columns = {'species_name':'species'})
-    #iris_df = pd.get_dummies(data = iris_df, columns = ['species'], drop_first=True)
+    if dummies == True:
+        iris_df = pd.get_dummies(data = iris_df, columns = ['species'], drop_first=True)
     return iris_df
-
-def train_validate_test_split(df, target, seed=123):
-    '''
-    This function takes in a dataframe, the name of the target variable
-    (for stratification purposes), and an integer for a setting a seed
-    and splits the data into train, validate and test. 
-    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
-    original dataset, and train is .70*.80= 56% of the original dataset. 
-    The function returns, in this order, train, validate and test dataframes. 
-    '''
-    train_validate, test = train_test_split(df, test_size=0.2, 
-                                            random_state=seed, 
-                                            stratify=df[target])
-    train, validate = train_test_split(train_validate, test_size=0.3, 
-                                       random_state=seed,
-                                       stratify=train_validate[target])
-    return train, validate, test
 
 # # Remember to delte your titanic .csv if you've already run this, or else it will break 
 def impute_mode(train, validate, test):
@@ -40,7 +24,7 @@ def impute_mode(train, validate, test):
     validate[['embark_town']] = imputer.transform(validate[['embark_town']])
     test[['embark_town']] = imputer.transform(test[['embark_town']])
     return train, validate, test
-    
+    t
 def prep_titanic_data(df):
     '''
     takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned dataframe
