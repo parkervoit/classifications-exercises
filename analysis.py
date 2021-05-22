6f772ebcfa791bac447dd6705c804bb51d326a40
parkervoit <parker.r.voit@gmail.com>
2021-05-22 11:31:45 -0500

    added an anlysis function file

---
+++ analysis.py
@@ -0,0 +1,47 @@
+def run_metrics(model, data_set):
+    """
+    This function takes in a model and ouputs metrics. 
+    model = name of class model
+    data_set = train, validate, test (AS A STRING)
+    Will output the Precision Score, the classification report, and the confusion matrix
+    It is advisable to print the name of the model you're working with before hand for clarity
+    i.e. print('Metrics for Model 1 with Train data\n')
+    """
+    if data_set == 'train':
+        X = X_train
+        y = y_train
+        df = train
+    if data_set == 'validate':
+        X = X_validate
+        y = y_validate
+        df = validate
+    if data_set == 'test':
+        X = X_test
+        y = y_test
+        df = test
+    score = model.score(X, y)
+    matrix = confusion_matrix(y, model.predict(X))
+    tpr = matrix[1,1] / (matrix[1,1] + matrix[1,0])
+    fpr = matrix[0,1] / (matrix[0,1] + matrix[0,0])
+    tnr = matrix[0,0] / (matrix[0,0] + matrix[0,1])
+    fnr = matrix[1,0] / (matrix[1,1] + matrix[1,0])
+    prc = matrix[1,1] / (matrix[1,1] + matrix[0,1])
+    
+    print(f'{data_set} data set accuracy score: {score:.2%}')
+    print(f'{data_set} data set precision score {prc:.2%}')
+    print(f'{data_set} data set recall score: {tpr:.2%}')
+    class_report = classification_report(y, model.predict(X), zero_division=True)
+    print('-------------------------------')
+    print(f'classification report')
+    print(class_report)
+    print ('-------------------------------')
+    print('')
+    print('confusion matrix')
+    print(matrix)
+    print(' ')
+    print(f'{data_set} data set model metrics')
+    print('---------------------------------')
+    print(f'True positive rate for the model is {tpr:.2%}')
+    print(f'False positive rate for the model is  {fpr:.2%}')
+    print(f'True negative rate for the model is {tnr:.2%}')
+    print(f'False negative rate for the model is {fnr:.2%}')
